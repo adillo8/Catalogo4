aaa18a641242f636f972cd8b00fced3e3457bbb6
adillo8 <pilar.fernandezadillo@usp.ceu.es>
2023-12-11 18:21:17 +0100

    "manifest y metodos borrar"

--- src/interfaz/interfaz/Interfaz2.java
+++ src/interfaz/interfaz/Interfaz2.java
@@ -47,14 +47,40 @@ public class Interfaz2 {
             System.out.println(juegos.get(i).toString());
         }
     }
+
+    public void borrarConsola(){
+        System.out.println("Introduzca el nombre de la consola que desea borrar");
+        //usando el scanner y bucle for para hacer algo distinto a lo que hemos hecho en clase
+        String nombre = sc.next();
+        for (int i = 0; i < consolas.size(); i++) {
+            if (consolas.get(i).getNombre().equals(nombre)) {
+                consolas.remove(i);
+            }
+        }
+    }
+
+    public void borrarJuego(){
+        System.out.println("Introduzca el nombre del juego que desea borrar");
+        String nombre = sc.next();
+        for (int i = 0; i < juegos.size(); i++) {
+            if (juegos.get(i).getNombre().equals(nombre)) {
+                juegos.remove(i);
+            }
+        }
+    }
     public void menu() {
         int opcion = 0;
         do {
+            System.out.println("-----MENU-----");
+            System.out.println("Bienvenido a la tienda de consolas");
+            System.out.println("¿Que desea hacer?");
             System.out.println("1. Añadir consola");
             System.out.println("2. Añadir juego");
             System.out.println("3. Mostrar consolas");
             System.out.println("4. Mostrar juegos");
-            System.out.println("5. Salir");
+            System.out.println("5. Borrar consola");
+            System.out.println("6. Borrar juego");
+            System.out.println("7. Salir");
             opcion = sc.nextInt();
             switch (opcion) {
                 case 1:
@@ -70,7 +96,13 @@ public class Interfaz2 {
                     mostrarJuegos();
                     break;
                 case 5:
-                    System.out.println("Gracias por usar el programa :)");
+                    borrarConsola();
+                    break;
+                case 6:
+                    borrarJuego();
+                    break;
+                case 7: 
+                    System.out.println("No te vayas plis :,3");
                     break;
                 default:
                     System.out.println("Opcion incorrecta :(");
